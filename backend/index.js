2bce213542138f26302587f1431c53e923b21171
Snehagupta1907 <snehagupta98930@gmail.com>
2024-02-01 01:32:57 +0530

    api created

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import csv from 'csv-parser';
@@ -9,34 +9,41 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true })); 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const originalData = [];
+
 fs.createReadStream('time_data.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        const dataPoint = {
-            timestamp: row['Timestamp'],
-            profitPercentage: parseFloat(row['Profit Percentage']),
-        };
-        originalData.push(dataPoint);
+  .pipe(csv())
+  .on('data', (row) => {
+    const dataPoint = {
+      timestamp: row['Timestamp'],
+      profitPercentage: parseFloat(row['Profit Percentage']),
+    };
+    originalData.push(dataPoint);
+  })
+  .on('end', () => {
+    console.log('Original data loaded');
+  });
 
-        if (originalData.length <= 5) {
-            console.log('Data Point:', dataPoint);
-        }
-    })
-    .on('end', () => {
-        console.log('Original data loaded');
-    });
+  app.get('/plot', (req, res) => {
+    const { timestamps, profitPercentages } = downsampleTimeSeries(
+        originalData.map((item) => item.timestamp),
+        originalData.map((item) => item.profitPercentage),
+        3000
+    );
+
+    console.log(`Downsampled data length: ${timestamps.length}`);
+
+    const downsampledData = timestamps.map((timestamp, index) => ({
+        timestamp,
+        profitPercentage: profitPercentages[index],
+    }));
+    res.json(downsampledData);
+});
 
 
-
-
-app.get('/',(req,res)=>{
-    res.send("hello sent");
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
